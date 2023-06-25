29d4d9311453d02917c24e20dbee294a77822c64
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2023-06-25 19:04:11 +0100

    Basic Cooklang parser import

--- rust_lib/build.rs
+++ rust_lib/build.rs
@@ -1,3 +1,3 @@
 fn main() {
-    uniffi::generate_scaffolding("./src/reverse.udl").unwrap();
+    uniffi::generate_scaffolding("./src/cooklang.udl").unwrap();
 }
--- rust_lib/src/lib.rs
+++ rust_lib/src/lib.rs
@@ -1,27 +1,19 @@
-uniffi::include_scaffolding!("reverse");
+// use cooklang::{error::PassResult, CooklangParser};
 
-pub fn reverse_string(input_string: &str) -> String {
-    input_string.chars().rev().collect()
-}
+uniffi::include_scaffolding!("cooklang");
 
-pub fn reverse_integer(input_integer: i32) -> i32 {
-    let reversed = input_integer.to_string().chars().rev().collect::<String>();
-    reversed.parse::<i32>().unwrap()
-}
+pub use std::{ops::RangeInclusive};
+pub use cooklang::{parse, error::*, model::*, quantity::*, metadata::*};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_reverses_strings() {
-        let result = reverse_string("hello world");
-        assert_eq!(result, "dlrow olleh");
+impl UniffiCustomTypeConverter for usize {
+    type Builtin = usize;
+
+    fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> {
+        Ok((val as u64).try_into().unwrap())
     }
 
-    #[test]
-    fn it_reverses_integers() {
-        let result = reverse_integer(123);
-        assert_eq!(result, 321);
+    fn from_custom(obj: Self) -> Self::Builtin {
+        usize::try_from(obj).unwrap()
     }
 }
