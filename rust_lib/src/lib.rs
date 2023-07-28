aac7da6c9f96eae4959e7ae041efd551c00dd95a
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2023-07-28 21:56:59 +0100

    Next

--- rust_lib/src/lib.rs
+++ rust_lib/src/lib.rs
@@ -6,14 +6,3 @@ pub use std::{ops::RangeInclusive};
 pub use cooklang::{parse, error::*, model::*, quantity::*, metadata::*};
 
 
-impl UniffiCustomTypeConverter for usize {
-    type Builtin = usize;
-
-    fn into_custom(val: Self::Builtin) -> uniffi::Result<Self> {
-        Ok((val as u64).try_into().unwrap())
-    }
-
-    fn from_custom(obj: Self) -> Self::Builtin {
-        usize::try_from(obj).unwrap()
-    }
-}
